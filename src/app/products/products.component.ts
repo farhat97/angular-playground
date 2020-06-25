662d4fb66ceeb4ef905e204f50da65114474e3f5
farhat97 <44099258+farhat97@users.noreply.github.com>
2020-06-25 15:07:42 -0500

    Initial Commit - Functional Product List

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { IProduct } from '../shared/interfaces';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  productList: IProduct[] = [
+    { id: 1, productName: "Fender Telecaster", productPrice: "600" },
+    { id: 2, productName: "G&L Asat Classic", productPrice: "1000" },
+    { id: 3, productName: "Minimoog Modular", productPrice: "3000" },
+    { id: 4, productName: "Korg Volca Beats", productPrice: "200" }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
